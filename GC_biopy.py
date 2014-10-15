23d2d107a48c7055efd7c3170115f7cbc0225f44
sls-kwan <shu.lun.shannon.kwan@gmail.com>
2014-10-15 13:23:44 +1100

    Implement

--- GC_biopy.py
+++ GC_biopy.py
@@ -1,6 +1,7 @@
 #this is a python script for finding GC content
 from Bio import SeqIO
 from Bio.SeqUtils import GC
+from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 from Bio.SeqUtils import CodonUsage
 from Bio.Alphabet import generic_dna, generic_protein
@@ -22,7 +23,8 @@ input_handle.close()
 
 dna_seq = {}
 for record in SeqIO.parse("BIN1.fasta", "fasta"):
-	seq = Seq(str(record), generic_dna)
+	seq = Seq(str(record.seq), generic_dna)
 	dna_seq[record.id] = seq.complement()
 for a_key, a_value in dna_seq.iteritems():
 	print a_key, a_value
+
